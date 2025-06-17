3570629091eb2145cdc72ba9260a159d01db327d
Jose-Adryan <josa.fernandez@duocuc.cl>
2025-06-17 16:41:31 -0400

    Add files via upload

---
+++ a.bash
@@ -0,0 +1,3 @@
+bash
+git clone https://github.com/rpm-software-management/dnf-plugins-core.git
+git commit -m "Hola mundo"
---
+++ par_o_impar.py
@@ -0,0 +1,5 @@
+numero=int(input("Ingrese su numero:"))
+if numero%2==0:
+    print("Es Par")
+else:
+    print("Es impar")
